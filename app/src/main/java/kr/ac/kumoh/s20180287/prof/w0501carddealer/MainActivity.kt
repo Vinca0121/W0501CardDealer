8bf7dbee4390ec66d341ae055bed03e8ca86a35a
Vinca0121 <qqw7820@gmail.com>
2022-10-11 13:53:18 +0900

    second commit✨

--- app/src/main/java/kr/ac/kumoh/s20180287/prof/w0501carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180287/prof/w0501carddealer/MainActivity.kt
@@ -2,10 +2,39 @@ package kr.ac.kumoh.s20180287.prof.w0501carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kumoh.s20180287.prof.w0501carddealer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding .card1.setImageResource(R.drawable.c_10_of_clubs)
+
+
+    }    private fun getCardName(c: Int) : String {
+        val shape = when (c / 13) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+
+        val number = when (c % 13) {
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+        return "c_${number}_of_${shape}"
     }
+
 }
